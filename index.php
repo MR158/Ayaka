88c8c2b0db0c08d75bef2f340995b9270c833c2e
MR158 <774356484@qq.com>
2022-03-20 21:39:10 +0900

    update: 添加头部栏波浪效果

--- index.php
+++ index.php
@@ -12,6 +12,20 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 $this->need('header.php');?>
 <header class="index-header">
     <div class="index-header__bg top-bg" style="background-image:url(<?php $this->options->themeUrl('/img/default_bg.jpg'); ?>)"></div>
+    <div class="index-header__waves top-waves">
+        <svg class="waves" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+        viewBox="0 24 150 28" preserveAspectRatio="none" shape-rendering="auto">
+            <defs>
+                <path id="gentle-wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
+            </defs>
+            <g class="parallax">
+                <use xlink:href="#gentle-wave" x="48" y="0" fill="rgba(252,252,252,0.7" />
+                <use xlink:href="#gentle-wave" x="48" y="3" fill="rgba(252,252,252,0.5)" />
+                <use xlink:href="#gentle-wave" x="48" y="5" fill="rgba(252,252,252,0.3)" />
+                <use xlink:href="#gentle-wave" x="48" y="7" fill="#fff" />
+            </g>
+        </svg>
+    </div>
 </header>
 
 <section class="profile">
